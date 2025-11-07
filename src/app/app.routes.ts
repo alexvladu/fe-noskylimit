71147f97f0c2992cc0ac89e8eb09b4b61a8d84a6
rafael <n.rafael2005@gmail.com>
2025-11-07 19:20:36 +0200

    aux changes 2

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,10 +1,12 @@
 import { Routes } from '@angular/router';
 import { LoginComponent } from './components/login/login';
 import { InboxComponent } from './components/inbox/inbox';
+import { HomeComponent } from './components/home/home';
 
 export const routes: Routes = [
   { path: 'login', component: LoginComponent },
   {path: 'inbox', component: InboxComponent},
+  {path: 'home', component:HomeComponent},
   { path: '', redirectTo: 'inbox', pathMatch: 'full' }, // redirecționare implicită
   { path: '**', redirectTo: 'inbox' } // fallback
 ];
